320cffece929166f9af589bcd715bc7b2ce48301
asktheaxis <alex.chrystal@encore.tech>
2020-11-09 15:24:49 -0500

    Adding actions/scripts to generate error emails for failing tasks/workflows

---
+++ actions/build_execution_tree.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# Copyright 2019 Encore Technologies
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from lib.base_action import BaseAction
+
+class BuildExecutionTree(BaseAction):
+
+	def run(self, **kwargs):
+
+		kwargs_dict = dict(kwargs)
+
+		st2_exe_id = self.get_arg("st2_exe_id", kwargs_dict, True)
+
+		vm_execution = self.st2_client_initialize(st2_exe_id)
+
+		if hasattr(parent_execution, 'children'):
+            for m in parent_execution.children:
+                self.get_execution_tree(m, delimeter)
+        else:
+            st2_executions = self.st2_client.executions  # pylint: disable=no-member
+            execution = st2_executions.get_by_id(parent_execution)
+            has_child = hasattr(execution, 'children')
+            symbol = '+> ' if has_child else '   '
+            task_name = delimeter + symbol + execution.context['orquesta']['task_name']
+            task_dict = {
+                'name': "<pre><code>{0}</pre></code>".format(task_name),
+                'status': execution.status
+            }
+            self.task_list.append(task_dict)  # pylint: disable=no-member
+            if has_child:
+                for c in execution.children:
+                    self.get_execution_tree(c, delimeter + '   ')
